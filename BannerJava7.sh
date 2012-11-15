f38341e5bc2957b09e2bdc07f89058751ded6923
Luke Andrews <leandrews@liberty.edu>
2012-11-15 14:20:16 -0500

    Changed from bash to shell

--- BannerJava7.sh
+++ BannerJava7.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 sed -i -e 's/deployment.javaws.jre.0.args=/deployment.javaws.jre.0.args=-Djava.vendor=\"Sun Microsystems Inc."/g' ~/Library/Application\ Support/Oracle/Java/Deployment/deployment.properties
